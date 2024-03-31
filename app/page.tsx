4bc1ec18c7740fada9be90bcea35512f178cdfcb
dylan72 <dylan.bouttefeux@hotmail.com>
2024-04-01 00:09:58 +0200

    V2

--- app/page.tsx
+++ app/page.tsx
@@ -24,7 +24,7 @@ export default function Home() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-center p-24">
             <h1 className="text-2xl font-bold mb-4">Données :</h1>
-            <pre>{JSON.stringify(donnees, null, 2)}</pre> {/* Données brutes formatées */}
+            <pre>{JSON.stringify(donnees, null, 2)}</pre> {/* Donnée brutes formatée */}
         </main>
     );
 }
